1a2aec9888a036ce062ad3d9aff5b92cd06d7350
Ivan Ovsyannikov <ov-iv@yandex.ru>
2022-08-24 21:26:56 +0300

    add Task 36

---
+++ HomeWork/Task36/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 36: Задайте одномерный массив,
+// заполненный случайными числами. 
+// Найдите сумму элементов, 
+// стоящих на нечётных позициях (индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length -1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void SumPolElem(int[] array)
+{    
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0) sum += array[i];
+    }
+    Console.Write($" -> {sum}");
+}
+
+int[] arr = CreateArrayRndInt(4, -100, 100);
+PrintArray(arr);
+SumPolElem(arr);
