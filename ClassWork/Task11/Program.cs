d1beea7b1fd34511955997baafcef70cb866ba13
Ivan Ovsyannikov <ov-iv@yandex.ru>
2022-08-30 20:20:55 +0300

    Task 41

--- ClassWork/Task11/Program.cs
+++ ClassWork/Task11/Program.cs
@@ -6,6 +6,49 @@
 // Console.WriteLine(number + " " + sot + " " + dec + " " + ed);
 // Console.WriteLine(result);
 
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     Random rnd = new Random();
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = rnd.Next(min, max + 1);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[ ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(i < array.Length -1) Console.Write($"{array[i]}, ");
+//         else Console.Write(array[i]);
+//     }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+// int[] GetSumPosNegElem(int[] array)
+// {
+//     int sumNeg = 0;
+//     int sumPos = 0;
+    
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] < 0) sumNeg += array[i];
+//         else sumPos += array[i];
+//     }
+//     return new int[]{sumNeg, sumPos};
+// }
+
+// int[] arr = CreateArrayRndInt(12, -9, 9);
+// PrintArray(arr);
+// int[] sumPosNegElem = GetSumPosNegElem(arr);
+// Console.WriteLine($"Сумма отрицательных чисел = {sumPosNegElem[0]}");
+// Console.WriteLine($"Сумма положительных чисел = {sumPosNegElem[1]}");
+
 int[] CreateArrayRndInt(int size, int min, int max)
 {
     int[] array = new int[size];
@@ -30,21 +73,23 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
-int[] GetSumPosNegElem(int[] array)
+void ReverseArray (int[] array)
 {
-    int sumNeg = 0;
-    int sumPos = 0;
-    
-    for (int i = 0; i < array.Length; i++)
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size - 1;
+    while (index1 < index2)
     {
-        if(array[i] < 0) sumNeg += array[i];
-        else sumPos += array[i];
+        int obj = array[index1];
+        array[index1] = array[index2];
+        array[index2] = obj;
+
+        index1++;
+        index2--;
     }
-    return new int[]{sumNeg, sumPos};
 }
 
-int[] arr = CreateArrayRndInt(12, -9, 9);
+int[] arr = CreateArrayRndInt(5,1,9);
 PrintArray(arr);
-int[] sumPosNegElem = GetSumPosNegElem(arr);
-Console.WriteLine($"Сумма отрицательных чисел = {sumPosNegElem[0]}");
-Console.WriteLine($"Сумма положительных чисел = {sumPosNegElem[1]}");
+ReverseArray(arr);
+PrintArray(arr);
---
+++ HomeWork/Task41/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 41: Пользователь вводит с
+// клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0
+// ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите {size} числа");
+int[] ReadArray(int count)
+{
+    int[] arr = new int[count];
+    int i = 0;
+    while (i < count)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        i++;
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length -1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+}
+
+void CountNumPolInArray (int[] array)
+{
+    int len = array.Length;
+    int i = 0;
+    int count = 0;
+    while (i < len)
+    {
+        if (array[i] > 0) count++;
+        i++;
+    }
+    Console.WriteLine($"-> {count}");
+}
+
+int[] array = ReadArray(size);
+
+PrintArray(array);
+CountNumPolInArray(array);
