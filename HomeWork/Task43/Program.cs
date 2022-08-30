afd4664952b1e5ac6a8524e35adef8f66385a229
Ivan Ovsyannikov <ov-iv@yandex.ru>
2022-08-30 21:19:13 +0300

    Task 43

--- HomeWork/Task43/Program.cs
+++ HomeWork/Task43/Program.cs
@@ -1,7 +1,24 @@
 ﻿// Задача 43: Напишите программу, 
 // которая найдёт точку пересечения
 // двух прямых, заданных уравнениями
-// y = k1 * x + b1, y = k2 * x + b2; 
-// значения b1, k1, b2 и k2 задаются
+// y = k1 * x + b1, y = k2 * x + b2; //  x = (b2 - b1)/(k1 - k2); y = k1 * ((b2 - b1)/(k1 - k2)) + b1;
+// значения b1, k1, b2 и k2 задаются //
 // пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите коэффиценты b1, k1, b2 и k2 для уравнений y = k1 * x + b1, y = k2 * x + b2;");
+Console.WriteLine("Введите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+if (k1 == k2) Console.WriteLine("Точки пересечения нет");
+else
+{
+    double x = (b2 - b1)/(k1 - k2);
+    double y = k1 * ((b2 - b1)/(k1 - k2)) + b1;
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x:#.#}; {y:#.#})");
+}
