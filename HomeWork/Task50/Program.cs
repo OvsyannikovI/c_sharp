4ade3a63624de0d4bb56e712f6a26aade45fee07
Ivan Ovsyannikov <ov-iv@yandex.ru>
2022-08-30 22:42:25 +0300

    Task 50

---
+++ HomeWork/Task50/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 50. Напишите программу,
+//  которая на вход принимает позиции
+//   элемента в двумерном массиве,
+//    и возвращает значение этого
+//     элемента или же указание,
+//      что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+int[,] matrix = new int[3, 4] {{1, 4, 7, 2},
+                                {5, 9, 2, 3},
+                                {8, 4, 2, 4}};
+
+Console.WriteLine("Введите позицию элемента матрицы m и n");
+Console.Write("m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+void PrintElemMatrix(int row, int col, int[,] matr)
+{
+    if (row > matr.GetLength(0) || col > matr.GetLength(1) || row < 0 || col < 0) Console.WriteLine($"{row}, {col} -> такого элемента в массиве нет");
+    else 
+    {
+        Console.WriteLine($"{row}, {col} -> {matr[row,col]}");
+    }
+}
+PrintElemMatrix(m,n, matrix);
